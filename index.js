ce0a867d43cd815757aa4ebc6ccb6d54643b4e2a
Rahul Kr. Singh <Rahul Kr. Singh@DESKTOP-SSS6T9N>
2020-06-24 20:30:19 +0530

    Assignment1 DishId changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
@@ -19,23 +20,23 @@ app.all('/dishes',(req, res, next) => {
         next();
 });
 
-app.get('/dishes/:dishId',(req,res,next) => {
-    res.end('will send details of the dish: ' +req.params.dishId + ' to you!');
-});
+// app.get('/dishes/:dishId',(req,res,next) => {
+//     res.end('will send details of the dish: ' +req.params.dishId + ' to you!');
+// });
 
-app.post('/dishes/:dishId', (req,res,next) =>{
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /dishes/${req.params.dishId}`);
-});
+// app.post('/dishes/:dishId', (req,res,next) =>{
+//     res.statusCode = 403;
+//     res.end(`POST operation not supported on /dishes/${req.params.dishId}`);
+// });
 
-app.put('/dishes/:dishId',(req,res,next) =>{
-    res.write(`Updating the dish: ${req.params.dishId} \n`);
-    res.end(`Will update the dish: ${req.body.name} with details ${req.body.description}`);
-});
+// app.put('/dishes/:dishId',(req,res,next) =>{
+//     res.write(`Updating the dish: ${req.params.dishId} \n`);
+//     res.end(`Will update the dish: ${req.body.name} with details ${req.body.description}`);
+// });
 
-app.delete('/dishes/:dishId', (req,res,next) =>{
-  res.end(`Deleting dish: ${req.params.dishId}`);
-});
+// app.delete('/dishes/:dishId', (req,res,next) =>{
+//   res.end(`Deleting dish: ${req.params.dishId}`);
+// });
 
 app.use('/dishes',dishRouter);
 
